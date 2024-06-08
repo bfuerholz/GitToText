9b3ce6d4c32b1eddbcf151bad5a15ba3a40197a4
Benedikt Fürholz <bfuerholz@gmail.com>
2024-06-08 14:54:52 +0200

    chore: Update react-scripts to version 5.0.1 in package.json

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -11,4 +12,7 @@ root.render(
   </React.StrictMode>
 );
 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
